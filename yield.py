53c3935728f9c420e0691e2cfaa6a3c0fdc22ca3
Bhavikaj97 <57636733+Bhavikaj97@users.noreply.github.com>
2021-03-25 14:49:09 +0530

    Update yield.py
    changed next Square to next Cube

--- yield.py
+++ yield.py
@@ -1,16 +1,16 @@
-def nextSquare():
+def nextCube():
     i = 1;
 
     # An Infinite loop to generate squares
     while True:
-        yield i * i
+        yield i * i * i
         i += 1  # Next execution resumes
         # from this point
 
 
 # Driver code to test above generator
 # function
-for num in nextSquare():
+for num in nextCube():
     if num > 100:
         break
     print(num)
